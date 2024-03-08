72cfe40f4c4c9a92a8e120c36976d14ed7d1a280
Piotr Sułek <piotrsulek123@gmail.com>
2024-03-08 17:47:31 +0100

    sd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
+public class Main {
+    int columnCount = 0;
+    boolean firstObject = true;
+    public static void main(String[] args) {
+        File inputFile = new File("mpp1/iris_training.txt");
+        try {
+            Scanner input = new Scanner(inputFile);
+            String entry = input.nextLine();
+            createTest(entry);
+
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Test createTest(String entry){
+        String name;
+        Double[] tab;
+        entry = entry.replace(" ","");
+        int count = 0;
+
+        String[] data = entry.split("\t");
+        for (String t:data) {
+            System.out.println(t);
+        }
+        return null;
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Test {
+import java.util.ArrayList;
+
+public class Test {
+    private ArrayList<Double> measurementsList;
+    private String conclusion;
+
+    public Test(Double[] pomiary,String conclusion){
+        this.conclusion = conclusion;
+        for (int i = 0; i < pomiary.length; i++) {
+            measurementsList.add(pomiary[i]);
+        }
+    }
+
+
 }
